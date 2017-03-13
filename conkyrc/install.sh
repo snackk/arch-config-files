dd5f56f53c087bd2efcda21230e7b3195b66a870
snackk <santos_matrix@sapo.pt>
2017-03-13 22:09:45 +0000

    Fixed bug on conkyrc

--- conkyrc/install.sh
+++ conkyrc/install.sh
@@ -1,1 +1,2 @@
+rm /home/$USER/.config/autostart/Conky.desktop
 ln -s /home/$USER/git/config-files/conkyrc/Conky.desktop /home/$USER/.config/autostart/Conky.desktop
--- tilda/install.sh
+++ tilda/install.sh
@@ -1,1 +1,4 @@
+rm /home/$USER/.config/tilda/config_0
+rm /home/$USER/.config/autostart/Tilda.desktop
 ln -s /home/$USER/git/config-files/tilda/tilda_config /home/$USER/.config/tilda/config_0
+ln -s /home/$USER/git/config-files/tilda/Tilda.desktop /home/$USER/.config/autostart/Tilda.desktop
