22943e67d94eca5ad09db40fddb0772224c8f853
snackk <santos_matrix@sapo.pt>
2017-03-13 23:12:44 +0000

    Conkyrc install doesn't work. TOFIX.

--- conkyrc/install.sh
+++ conkyrc/install.sh
@@ -1,7 +1,5 @@
-rm $HOME/.config/autostart/Conky.desktop
-ln -s $HOME/git/config-files/conkyrc/Conky.desktop $HOME/.config/autostart/Conky.desktop
 ln -s $HOME/git/config-files/conkyrc/battery.sh $HOME/.config/autostart/battery.sh
 ln -s $HOME/git/config-files/conkyrc/raspberry_temp.sh $HOME/.config/autostart/raspberry_temp.sh
 ln -s $HOME/git/config-files/conkyrc/sensors.sh $HOME/.config/autostart/sensors.sh
 ln -s $HOME/git/config-files/conkyrc/time.sh $HOME/.config/autostart/time.sh
-chmod +x $HOME/.config/autostart/battery.sh $HOME/.config/autostart/raspberry_temp.sh $HOME/.config/autostart/sensors.sh $HOME/.config/autostart/time.sh
+ln -s $HOME/git/config-files/conkyrc/conkyrc $HOME/.config/autostart/conkyrc
