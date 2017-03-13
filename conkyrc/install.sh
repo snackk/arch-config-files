30b79fa2067aa1a94b5433daf6ecf4d160ad4d87
snackk <santos_matrix@sapo.pt>
2017-03-13 22:05:26 +0000

    Added most of configs with install.sh

---
+++ conkyrc/install.sh
@@ -0,0 +1,1 @@
+ln -s /home/$USER/git/config-files/conkyrc/Conky.desktop /home/$USER/.config/autostart/Conky.desktop
---
+++ zsh/install.sh
@@ -0,0 +1,5 @@
+rm /home/$USER/.zshrc
+rm /home/$USER/.oh-my-zsh/custom/themes 
+ln -s /home/$USER/git/config-files/zsh/zshrc /home/$USER/.zshrc
+mkdir /home/$USER/.oh-my-zsh/custom/themes 
+ln -s /home/$USER/git/config-files/zsh/oh-my-zsh/custom/themes/snackkster.zsh-theme /home/$USER/.oh-my-zsh/custom/themes/snackkster.zsh-theme
