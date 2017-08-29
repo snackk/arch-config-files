74f981e8feedb2635007d0a2f9cb84326022a36d
snackk <santos_matrix@sapo.pt>
2017-08-29 22:16:01 +0100

    Fixed small bug. Tilda folder didn't exist.

--- tilda/install.sh
+++ tilda/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir /$HOME/.config/tilda
 ln -s $HOME/git/config-files/tilda/tilda_config $HOME/.config/tilda/config_0
 ln -s $HOME/git/config-files/tilda/Tilda.desktop $HOME/.config/autostart/Tilda.desktop
