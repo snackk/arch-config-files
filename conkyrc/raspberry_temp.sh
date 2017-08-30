4cb46ef92a6c0f2cbd87743320e454656dcdde25
snackk <santos_matrix@sapo.pt>
2017-08-30 19:19:15 +0100

    Gay stuff LOL

--- conkyrc/raspberry_temp.sh
+++ conkyrc/raspberry_temp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 temp=$(ssh -p 1337 pi@owncloud.flnet.org "/opt/vc/bin/vcgencmd measure_temp" | cut -c6-9)°C
 #echo -n '${color2}'"ownCloudServer: "'${color}''${color3}'$temp'${color}'
-echo -n "ownCloudServer "'${color2}'$temp'${color}'
+echo -n "OWNCLOUD "'${color2}'$temp'${color}'
 
