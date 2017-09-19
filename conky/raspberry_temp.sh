d8b76bb5356dfa7993d831cb6561bb252a6d9682
snackk <santos_matrix@sapo.pt>
2017-09-19 22:12:25 +0100

    Added mark1 temps. Also added alias on zsh

--- conky/raspberry_temp.sh
+++ conky/raspberry_temp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-temp=$(ssh -p 1337 pi@owncloud.flnet.org "/opt/vc/bin/vcgencmd measure_temp" | cut -c6-9)°C
+temp_o=$(ssh -p 1337 pi@owncloud.flnet.org "/opt/vc/bin/vcgencmd measure_temp" | cut -c6-9)°C
+temp_m=$(ssh -p 1337 pi@mark1.darktech.org "/opt/vc/bin/vcgencmd measure_temp" | cut -c6-9)°C
 #echo -n '${color2}'"ownCloudServer: "'${color}''${color3}'$temp'${color}'
-echo -n "OWNCLOUD "'${color2}'$temp'${color}'
+echo -n "OWNCLOUD "'${color2}'$temp_o'${color}' " MARK1 "'${color2}'$temp_m'${color}'
 
