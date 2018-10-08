396e289197a4d3fb90b05af468d155cb2eaf9660
snackk <snackk026@gmail.com>
2018-10-08 23:50:10 +0100

    Added battery status again. Removed string time left

--- conky/time.sh
+++ conky/time.sh
@@ -4,10 +4,10 @@ batt_dis=$(echo $batt | cut -c12-22)
 batt_full=$(echo $batt | cut -c12-15)
 
 if [ "$batt_dis" == "Discharging" ]; then
-  echo -n "TIME-LEFT "'${color2}'$(echo $batt | cut -c30-34)H'${color}'
+  echo -n '${color2}'$(echo $batt | cut -c30-34)h'${color}'
 else
 	if [ "$batt_full" == "Full" ]; then
-  		echo -n "TIME-LEFT "'${color1}'"Battery full"'${color}'
-	else echo -n "TIME-LEFT "'${color2}'$(echo $batt | cut -c27-31)H'${color}'
+  		echo -n '${color1}'"Battery full"'${color}'
+	else echo -n '${color2}'$(echo $batt | cut -c27-31)h'${color}'
 	fi	
 fi
