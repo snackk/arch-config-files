f01f647e71263eeff3040649b7688c4c313bee5e
snackk <snackk026@gmail.com>
2018-10-08 12:22:42 +0100

    updated conky files

--- conky/raspberry_temp.sh
+++ conky/raspberry_temp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-temp_o=$(ssh -p 1337 pi@owncloud.flnet.org "/opt/vc/bin/vcgencmd measure_temp" | cut -c6-9)°C
-temp_m=$(ssh -p 1337 pi@mark1.darktech.org "/opt/vc/bin/vcgencmd measure_temp" | cut -c6-9)°C
-#echo -n '${color2}'"ownCloudServer: "'${color}''${color3}'$temp'${color}'
-echo -n "OWNCLOUD "'${color2}'$temp_o'${color}' " MARK1 "'${color2}'$temp_m'${color}'
+temp_o=$(ssh -p 1337 pi@snackkdrive.ddns.net "/opt/vc/bin/vcgencmd measure_temp" | cut -c6-9)°C
+#echo -n "SNACKKDRIVE "'${color2}'$temp_o'${color}' " MARK1 "'${color2}'$temp_m'${color}'
+echo -n "SNACKKDRIVE "'${color2}'$temp_o'${color}'
 
