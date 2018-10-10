166dceaceae58182b7669cf6601d982d07b8b2eb
snackk <snackk026@gmail.com>
2018-10-10 20:01:45 +0100

    fix #12

--- conky/laptop_battery.sh
+++ conky/laptop_battery.sh
@@ -6,19 +6,26 @@ batt=$(acpi | grep Battery)
 batt_dis=$(echo $batt | cut -c12-22)
 batt_full=$(echo $batt | cut -c12-15)
 batt_perc=$(echo $batt | cut -c25-26)
+batt_dis_zero_rate=$(echo $batt | cut -c45-48)
 
-if [ "$batt_dis" == "Discharging" ]; then
-	if [[ "$batt_perc" -le $MIN_BAT_THRESHOLD ]]; then 
-		print_label_string_color1 "BATTERY " "$(echo $batt_perc)% "
-	else 
-		print_label_string_color2 "BATTERY " "$(echo $batt_perc)% "
-	fi
-	print_label_string_color2 "" " $(echo $batt | cut -c30-34)H"
+if [ "$batt_dis_zero_rate" == "zero" ]; then
+	print_label_string_color1 "BATTERY " "CHARGED"
 else
-	if [ "$batt_full" == "Full" ]; then
-  		print_label_string_color1 "BATTERY " "CHARGED"
-	else 
-		print_label_string_color2 "BATTERY " "CHARGING "
-		print_label_string_color2 "" " $(echo $batt | cut -c27-31)H"
-	fi	
+	if [ "$batt_dis" == "Discharging" ]; then
+		if [[ "$batt_perc" -le $MIN_BAT_THRESHOLD ]]; then 
+			print_label_string_color1 "BATTERY " "$(echo $batt_perc)% "
+		else 
+			print_label_string_color2 "BATTERY " "$(echo $batt_perc)% "
+		fi
+	
+	print_label_string_color2 "" " $(echo $batt | cut -c30-34)H"
+
+	else
+		if [ "$batt_full" == "Full" ]; then
+	  		print_label_string_color1 "BATTERY " "CHARGED"
+		else 
+			print_label_string_color2 "BATTERY " "CHARGING "
+			print_label_string_color2 "" " $(echo $batt | cut -c27-31)H"
+		fi	
+	fi
 fi
