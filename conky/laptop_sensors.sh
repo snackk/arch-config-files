f5fa41d82fdb5a63f67c081cf00cb40596221263
snackk <snackk026@gmail.com>
2018-10-10 02:31:18 +0100

    guess the -ge was right after all...

--- conky/laptop_sensors.sh
+++ conky/laptop_sensors.sh
@@ -5,13 +5,13 @@ source $HOME/git/arch-config-files/conky/conky_utils.conf
 cpu_temp=$(sensors | grep "Core 1:" | cut -c17-18)
 graphic_temp=$(sensors | grep id | cut -c17-18)
 
-if [[ "$cpu_temp" -le $MAX_TEMP_THRESHOLD ]]; then 
+if [[ "$cpu_temp" -ge $MAX_TEMP_THRESHOLD ]]; then 
 	print_label_string_color1 "CPU " "$(echo $cpu_temp)ºC "
 else
 	print_label_string_color2 "CPU " "$(echo $cpu_temp)ºC "
 fi	
 
-if [[ "$graphic_temp" -le $MAX_TEMP_THRESHOLD ]]; then 
+if [[ "$graphic_temp" -ge $MAX_TEMP_THRESHOLD ]]; then 
 	print_label_string_color1 " GRAPHICS " "$(echo $graphic_temp)ºC"
 else
 	print_label_string_color2 " GRAPHICS " "$(echo $graphic_temp)ºC"
